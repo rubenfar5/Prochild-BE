77016f52b93edd479340d444ddb7ddc9568331be
albanomelo1 <58079625+albanomelo1@users.noreply.github.com>
2021-03-31 15:59:36 +0100

    albano

--- src/main/java/DAI/Prochild/Controller/Lo.java
+++ src/main/java/DAI/Prochild/Controller/Lo.java
@@ -1,20 +1,21 @@
-package DAI.Prochild.Registration;
+package DAI.Prochild.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class Login {
+@RequestMapping("prochild/")
+public class Lo {
+
+    @GetMapping("registarConta")
+    public String registarPage() {
+        return "RegistarFamilia";
+    }
 
     @GetMapping("login")
     public String loginPage() {
-        return "Login";
+        return "LoginAdmin";
     }
 
-    @GetMapping("menu")
-    public String menuAdmin() {
-        return "MenuPrincipal";
-    }
 }
