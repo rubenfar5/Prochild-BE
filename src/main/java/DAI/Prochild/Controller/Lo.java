0a3ad1c0397068ca44af6149032e9bd90d7ec4e4
albanomelo1 <58079625+albanomelo1@users.noreply.github.com>
2021-04-01 12:12:17 +0100

    albano

--- src/main/java/DAI/Prochild/Controller/Lo.java
+++ src/main/java/DAI/Prochild/Controller/Lo.java
@@ -1,8 +1,14 @@
 package DAI.Prochild.Controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("prochild/")
@@ -18,4 +24,18 @@ public class Lo {
         return "LoginAdmin";
     }
 
+    @GetMapping("menu")
+    public Object loginPageRedirect(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException, ServletException {
+        String role =  authResult.getAuthorities().toString();
+
+        //String caminho = "";
+        if(role.contains("ROLE_ADMIN")){
+            return "RegistarFamilia";
+        }
+        else if(role.contains("familia")) {
+            return "MenuPrincipal";
+        }
+
+        return null;
+    }
 }
